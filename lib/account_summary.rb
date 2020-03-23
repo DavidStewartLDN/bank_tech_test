7290874988c7ae7b85fa2fd767a98b8dcc1d19de
DavidStewartLDN <davidstewart.work@gmail.com>
2020-03-23 22:53:39 +0000

    add withdraw to store_transaction

--- lib/account_summary.rb
+++ lib/account_summary.rb
@@ -8,7 +8,11 @@ class AccountSummary
 
   def store_transaction(type, amount, total)
     date = Time.now.strftime("%d/%m/%Y")
-    statement_line = date.to_s + "||" + amount.to_s + "|| ||" + total.to_s
+    if type == "deposit"
+      statement_line = date + "||" + amount.to_s + "|| ||" + total.to_s
+    elsif type == "withdraw"
+      statement_line = date + "|| ||" + amount.to_s + "||" + total.to_s
+    end
     @transaction_history.push(statement_line)
   end
 
--- spec/account_summary_spec.rb
+++ spec/account_summary_spec.rb
@@ -5,8 +5,15 @@ describe AccountSummary do
     it "it should take in transaction information and store in an array transaction_history" do
       date = Time.now.strftime("%d/%m/%Y")
       total = 1000
-      subject.store_transaction("deposit", 1000, 1000)
+      subject.store_transaction("deposit", 1000, total)
       expect(subject.transaction_history[0]).to eq ("#{date}||1000|| ||1000")
+    end
+
+    it "it should take in transaction information and store in an array transaction_history" do
+      date = Time.now.strftime("%d/%m/%Y")
+      total = -1000
+      subject.store_transaction("withdraw", 1000, total)
+      expect(subject.transaction_history[0]).to eq ("#{date}|| ||1000||-1000")
     end
   end
 end
