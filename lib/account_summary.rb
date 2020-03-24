8e8904a79d0caf98f44ae81939269b0bf15e8c3c
DavidStewartLDN <davidstewart.work@gmail.com>
2020-03-24 12:57:06 +0000

    refactored store transaction to take out statement_line creation to seperate function - SRP

--- lib/account_summary.rb
+++ lib/account_summary.rb
@@ -8,12 +8,16 @@ class AccountSummary
 
   def store_transaction(type, amount, total)
     date = Time.now.strftime("%d/%m/%Y")
+    statement_line = create_statement_line(type, amount, total, date)
+    @transaction_history.push(statement_line)
+  end
+
+  def create_statement_line(type, amount, total, date)
     if type == "deposit"
       statement_line = date + "||" + amount.to_s + "|| ||" + total.to_s
     elsif type == "withdraw"
       statement_line = date + "|| ||" + amount.to_s + "||" + total.to_s
     end
-    @transaction_history.push(statement_line)
   end
 
   def print_statement
